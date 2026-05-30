fb9f166b84ddc310e7d327259b8510b4863998f9
Andrea Carreño <andrea.carreno@meetcard.cl>
2026-05-30 02:48:40 -0400

    se agrega database better-sqlite3 y api endp about y faqs get

---
+++ server/api/faqs/index.get.ts
@@ -0,0 +1,5 @@
+import Database from "better-sqlite3";
+export default defineEventHandler(() => {
+  const db = new Database(".data/database.sqlite");
+  return db.prepare("SELECT * FROM faqs ORDER BY id ASC").all();
+});

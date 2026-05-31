fcabe5dda0c8a50bf6994c29a5b58d328f31283b
Andrea Carreño <andrea.carreno@meetcard.cl>
2026-05-31 00:31:41 -0400

    corrección tipero servicesection en index

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,7 +2,7 @@
     <main>
         <HeroSection />
         <AboutSection />
-        <ServicesSection />
+        <ServiceSection />
         <!-- <TestimonialCarousel /> -->
         <FAQSection />
         <!-- <ContactSection /> -->
